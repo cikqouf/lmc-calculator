fbf7d1852d2fe7e0ff7cde5f74ddf621a6ceadb2
cckidouq <67586822+cikqouf@users.noreply.github.com>
2020-07-10 18:19:27 +0800

    Add files via upload

---
+++ 运算和赋值（试作型/运算试验.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+#include<string>
+#include<regex>
+using namespace std;
+main()
+{
+	string z;
+	cout<< "the expression is "; cin>> z;
+	regex p ("(\\+)"); //一个浅陋的思路，肯定会有性能损失
+	if (regex_search(z,p))
+	{
+		cout<< " + ";
+	}
+	system("pause");
+	return 0;
+}
+		
+
