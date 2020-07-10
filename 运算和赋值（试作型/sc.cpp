f3ff40c54be27f0a3a834d5b147d50f73271a54e
cckidouq <67586822+cikqouf@users.noreply.github.com>
2020-07-10 11:51:03 +0800

    Add files via upload

---
+++ 运算和赋值（试作型/sc.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+#include<string>
+using namespace std;
+main()
+{
+	string x,y;
+	cout<< "insert a expression" << endl;
+	cout<< "The former one "; cin>> x;
+	cout<< "the later one "; cin>> y;
+	if (x == y)
+	        {
+			cout<< "2 " << x;
+		}
+	system("pause");
+	return 0;
+}
+		
+
