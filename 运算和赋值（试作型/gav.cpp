a40828e094da67926b929c73833f83b9273c7971
cckidouq <67586822+cikqouf@users.noreply.github.com>
2020-07-10 12:20:50 +0800

    Add files via upload

---
+++ 运算和赋值（试作型/gav.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+#include<string>
+using namespace std;
+main()
+{
+        float x,z;
+	string y;
+	cout<< "The var is ";
+	cin>> y;
+	/*So you can set any var you want but now you can only set just one var in a time, which is really useless*/
+	/*What I want to do now is to connect x with y so that I can give a number to x, and meanwhile I can set y as its name*/
+       	cout<< "var = " << y << endl;
+	cout<< "Press 0 to give " << y << " a value or to press a number except 0 to ignore ";
+	cin>> z;
+	if(z==0)
+	{
+		cout<< "Value = "; cin>> x;
+	        cout<< y << " = " << x << endl;
+	}
+	else
+	{
+		cout<< "The expression is " << y << endl;
+	}	
+        system("pause");
+	return 0;
+}
+
+	
