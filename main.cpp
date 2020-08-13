621abca54b9c508e3fefe923c8882a116029e5cd
cckidouq <67586822+cikqouf@users.noreply.github.com>
2020-08-13 16:12:32 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,29 +3,42 @@
 using namespace std;
 string Ep;
 char p;
+int n = 0;
+int i =1;
 
-void inout()
+class computing
 {
-    int n = 0;
-    for(int i =1; n < i; i++)
-        {
-            cout<< "[In" << i << "]>>> " ; cin>> Ep;
-            cout<< "[Out" << i << "]<<< " << Ep <<endl;
-            cout<< "End? "; cin>> p;
+    public:
+
+        char plus = '+';
+        char minus = '-';
+        char multiply = '*';
+};
+
+void console()
+{
+    cout<< "quit? "; cin>> p;
             switch (p)
             {
                 case 'N':
                 n = 0;
                 break;
                 case 'Y':
-                n = 999;
+                n = i + 10;
                 break;
                 default:
                 cerr<< "wrong! ";
             }
+}
 
+void inout()
+{
+    for(int i =1; n < i; i++)
+        {
+            cout<< "[In" << i << "]>>> " ; cin>> Ep;
+            cout<< "[Out" << i << "]<<< " << Ep <<endl;
+            console();
         }
-            
 }
 
 int main()
